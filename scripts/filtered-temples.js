2066278c39cbca3466baa95cfd7d38221e4ebb2a
Prosper Anosike <odinakachukwu007@gmail.com>
2025-11-24 20:56:23 +0100

    Use alternative image source for Tokyo temple

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -64,7 +64,7 @@ const temples = [
     location: "Tokyo, Japan",
     dedicated: "1980, October, 27",
     area: 53997,
-    imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/tokyo-japan/400x250/tokyo-japan-temple-lds-933045-wallpaper.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/tokyo-japan-temple/tokyo-japan-temple-26340-main.jpg"
   },
   {
     templeName: "Rome Italy",
